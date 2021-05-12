3dc1f32c54af33f6576382b371060a72cdb397cc
admin <admin@2008-MV10>
2021-05-12 13:00:58 +0200

    Añadido el archivo updates.sql

--- CursoJava2021/src/modelo/sql/updates.sql
+++ CursoJava2021/src/modelo/sql/updates.sql
@@ -1,1 +1,4 @@
 update figuras set fig_nombre="cuadrado1" where fig_id=1
+update figuras set fig_nombre="circulo1" where fig_id=2
+update figuras set fig_nombre="rectangulo1" where fig_id=4
+update figuras set fig_nombre="pentagono1" where fig_id=5
